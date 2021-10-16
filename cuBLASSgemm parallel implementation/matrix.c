e6acc7c154d26949a45e4a356c97d511a05fde5f
dlwar3 <71031540+dlwar3@users.noreply.github.com>
2021-10-15 23:08:03 -0500

    projects 5 and 6

---
+++ cuBLASSgemm parallel implementation/matrix.c
@@ -0,0 +1,101 @@
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <assert.h>
+  #include <math.h>
+  #include <time.h>
+
+  typedef struct matrix
+  {
+    int dim;
+    float *data;
+  } MATRIX;
+
+  MATRIX* newEmptyMATRIX(int dim)
+  {
+      MATRIX *m = malloc(sizeof(MATRIX));
+      assert(sizeof(m) > 0);
+      m->dim = dim;
+      m->data = (float*)calloc(sizeof(float), m->dim * m->dim);
+      assert(sizeof(m->data) > 0);  //make sure we don't have empty data
+      return m;
+  }
+
+  MATRIX* newMATRIXFromFile(char *file, int dim)
+  {
+    MATRIX *m = malloc(sizeof(MATRIX));
+    FILE *fp = fopen(file, "rb");
+    assert(fp != NULL);
+    m->dim = dim;
+    //allocate matrix
+    m->data = (float*)calloc(sizeof(float), m->dim * m->dim);
+    assert(sizeof(m->data) > 0);  //make sure we don't have empty data
+
+    //populate data array
+    int i;
+    for (i=0; i< dim * dim; i++)
+    {
+      
+      fread(&m->data[i], sizeof(float), 1, fp);
+      printf("%f\n", m->data[i]);
+    }
+    fclose(fp);
+    return m;
+  }
+
+  void generateNewMATRIXFile(char *file, int dim)
+  {
+      srand(time(NULL));
+      FILE *fp = fopen(file, "w");
+      assert(fp != NULL);
+      int i;
+      for (i = 0; i< dim * dim; i++)
+      {
+          fprintf(fp, "%f ", (double)rand());
+      }
+      fclose(fp);
+  }
+
+  void populateMATRIX(MATRIX *m)
+  {
+      srand(time(NULL));
+      int i;
+      for (i=0; i<m->dim * m->dim; i++)
+      {
+          m->data[i] = (double)rand();
+      }
+
+  }
+  void outputExistingMATRIXToFile(MATRIX *m, char *file)
+  {
+      FILE *fp = fopen(file, "w");
+      assert(fp != NULL);
+      int i;
+      for (i=0; i<m->dim * m->dim; i++)
+      {
+          fprintf(fp, "%f ", m->data[i]);
+      }
+      fclose(fp);
+  }
+
+  double findMaxDifference(MATRIX *a, MATRIX *b)
+  {
+    double maxDifference = 0.0;
+    double temp = 0.0;
+    int i;
+    for (i=0; i<a->dim * a->dim; i++)
+    {
+      temp = fabs(a->data[i] - b->data[i]);
+      if (temp > maxDifference)
+      {
+        maxDifference = temp;
+      }
+    }
+    return maxDifference;
+  }
+
+  void freeMATRIX(MATRIX *m)
+  {
+    free(m->data);
+    free(m);
+  }
+
